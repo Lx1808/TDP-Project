3aa5ab790d7ba7aa50dfc25fbfbec13a16f99dfb
EthanQinnn <permanenttree@gmail.com>
2024-05-16 00:07:42 +1000

    adjust translate

--- translator.py
+++ translator.py
@@ -2,9 +2,10 @@
 from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
 from langdetect import detect
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+import re
+
 translate_model = M2M100ForConditionalGeneration.from_pretrained("facebook/m2m100_418M")
 translate_tokenizer = M2M100Tokenizer.from_pretrained("facebook/m2m100_418M")
-
 # 加载模型和分词器
 tokenizer = AutoTokenizer.from_pretrained("papluca/xlm-roberta-base-language-detection")
 model = AutoModelForSequenceClassification.from_pretrained("papluca/xlm-roberta-base-language-detection")
@@ -25,4 +26,12 @@ def translate(text, target_lang):
     encoded_input = translate_tokenizer(text, return_tensors="pt")
     output_ids = translate_model.generate(**encoded_input, forced_bos_token_id=translate_tokenizer.get_lang_id(target_lang))
     output_text = translate_tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0]
-    return output_text
+    
+    print("output_text:",output_text)
+    pattern = r'^([^:]*:)(.*)'
+    new_text = remove_prefix(output_text,pattern)
+    print("new_text:",new_text)
+    return new_text
+
+def remove_prefix(text,pattern):
+    return re.sub(pattern, r'\2', text, flags=re.MULTILINE)
